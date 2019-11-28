f8a9858983c89f332913028926549a44dd27fb28
Adrian Vazquez <aavm0000@gmail.com>
2019-11-28 09:04:46 -0400

    First commit

---
+++ build/cxx/cxx_exception_stubs.d
@@ -0,0 +1,6 @@
+cxx_exception_stubs.o: \
+ /home/estelio/esp/esp-idf/components/cxx/cxx_exception_stubs.cpp \
+ /home/estelio/eclipse-workspace/Project1/build/include/sdkconfig.h
+
+/home/estelio/eclipse-workspace/Project1/build/include/sdkconfig.h:
+/home/estelio/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp:
